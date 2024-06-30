e05d19df5623ab88ae9cd77ec98770f57dbca8ab
hanie <hanieshahamat@gmail.com>
2024-06-30 22:22:23 +0330

    typescript counter app

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleInfo,faGear,faArrowsRotate } from '@fortawesome/free-solid-svg-icons';
+
+
+function App() {
+  const [count, setCount] = useState<number>(0)
+
+  return (
+    <>
+      <div className="flex items-center justify-center py-12 my-12">
+      <FontAwesomeIcon icon={faCircleInfo} className="ri-information-line text-gray-300 px-7" />
+      <FontAwesomeIcon icon={faGear} className="ri-settings-3-line text-gray-300" />
+      <FontAwesomeIcon icon={faArrowsRotate} className="ri-loop-right-line text-gray-300 px-7" onClick={()=>setCount(0)} />
+      </div>
+      <h1 className="text-gray-300 flex items-center justify-center text-9xl font-bold">{count}</h1>
+      <div className="text-gray-300 flex items-center justify-center py-12 mt-14">
+        <button className="rounded-full outline outline-offset-0 outline-gray-300 p-2 px-4 mr-20" onClick={() => setCount((count) => count + 1)}>
+          +
+        </button>
+        <button className="rounded-full outline outline-offset-0 outline-gray-300 p-2 px-5 ml-20" onClick={() => setCount((count) => (count<=0 ? 0 : count-1))}>
+          -
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
